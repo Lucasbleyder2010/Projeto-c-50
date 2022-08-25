6ae53e301058f7739c391e2583d867d3fee3c25b
Lucasbleyder2010 <100377923+Lucasbleyder2010@users.noreply.github.com>
2022-08-25 13:54:02 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,7 @@
+// meu objetivo é criar um jogo de batalha naval, vão ser 2 navios:
+//Sunny = Jogador
+//Merry = Pc
+//Um fundo do oceano e fazer os barcos jogarem bolas de canhão um no outro.
+// colocar o placar e o jogo vai acabar quando 1 deles for acertado com 3 canhões.
+// não pensei em colocar som
+// me basiei no Archery e no jogo do canhão
